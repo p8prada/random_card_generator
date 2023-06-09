7c69b79f11492f95f05395ec19d01807908acc5e
p8prada <131833040+p8prada@users.noreply.github.com>
2023-06-09 17:38:58 +0000

    new card, new car every 10 seconds and resize card

--- scr/app.js
+++ scr/app.js
@@ -70,3 +70,98 @@ function blackCard(x){
     }
 }
 
+var opt1 = document.getElementById("opt1_btn");
+var opt2 = document.getElementById("opt2_btn");
+var opt3 = document.getElementById("btn_resize");
+var cardAnimation = document.getElementById("card");
+
+// Runs the firts option: show a new card
+opt1.addEventListener("click" ,newCard);
+
+
+// Runs the second option: new card every 10 seconds
+opt2.addEventListener("click", function(){
+    startTime();
+    document.getElementById("time").style.display = "block";
+    document.getElementById("btn_stop").style.display = "block";
+});
+
+var intervalId;
+var timer = document.getElementById("time");
+var seconds = 10;
+
+// Start the 10 second timer
+function startTime(){
+    intervalId = setInterval(updateTimer, 1000);
+    updateTimer();
+}
+
+// Update the timer every second and when gets 0, create a new card
+function updateTimer(){
+    timer.textContent = seconds;
+    if(seconds === 0){
+        seconds = 10;
+        newCard();
+    }
+    document.getElementById("time").innerText = "  " + seconds;
+    seconds--;
+}
+
+// Stop the timer if the user click the button 'stop'
+document.getElementById("btn_stop").addEventListener("click",stopTimer);
+
+function stopTimer(){
+    clearInterval(intervalId);
+    document.getElementById("time").style.display = "none";
+    document.getElementById("btn_stop").style.display = "none";
+}
+
+
+// Runs the third option: resize the card
+opt3.addEventListener("click", function(){
+    
+    var width = document.getElementById("card_width").value;
+    var height = document.getElementById("card_height").value;
+    
+    document.getElementById("card").style.width = width + "px";
+    document.getElementById("card").style.height = height + "px";
+
+    newCard();
+});
+
+var default_size = document.getElementById("flexCheckDefault");
+default_size.addEventListener("change", function(){
+    if(this.checked){
+        document.getElementById("card_width").value = "226";
+        document.getElementById("card_width").disabled = true;
+        document.getElementById("card_height").value = "340";
+        document.getElementById("card_height").disabled = true;
+    }else{
+        document.getElementById("card_width").value = "";
+        document.getElementById("card_width").disabled = false;
+        document.getElementById("card_height").value = "";
+        document.getElementById("card_height").disabled = false;
+    }
+});
+
+
+// Generate a new card
+function newCard(){
+    genNewCard();
+    animation();
+}
+// Apply the card animation every time the card change
+function animation(){
+    cardAnimation.classList.remove("animated");
+    cardAnimation.classList.remove("animatedFadeInUp");
+    cardAnimation.classList.remove("fadeInUp");
+    setTimeout(function() {
+        cardAnimation.classList.add("animated");
+        cardAnimation.classList.add("animatedFadeInUp");
+        cardAnimation.classList.add("fadeInUp");
+    }, 1);
+}
+
+
+
+
